0af2d29a1552b45675b85c64d7a7e85fcbda2a34
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-07-12 13:00:44 +0530

    loader, slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,47 @@
-
-window.onscroll = function() {
-    var dv = document.getElementById('video-box');
-    var v = document.getElementById('i_video');
-    if ((window.scrollY < (dv.offsetTop + dv.offsetHeight)) && ((window.scrollY + window.outerHeight) > dv.offsetTop)) {
-        if (v.src=='' || v.src==location.href) {
-            v.src='VIDEO_URL';
-        }
-    } else {
-        v.src='';
-    }
+//loader
+function removeLoader(){
+    document.querySelector(".loader").style.display = "none";
 }
-window.onload(document.querySelector(".loader").style.display="none");
+window.onload=removeLoader;
+//
 
+//slider
+let text = ['Some text about the photo', 'Some random text about the photo', 'Some random text about the photo', 'Some random text about the photo',
+     'Some random text about the photo', 'Some random text about the photo', 'Some random text about the photo', 'Some random text about the photo'];
+let value;
+
+
+function slider(val){
+    document.querySelector(".slider").style.display="inline";
+    document.querySelector('.slider-photo').src="assets/img/image"+ val.toString()+".jpg";
+    document.querySelector('.slider-text').innerText=text[value-1];
+}
+
+let imgs=document.querySelectorAll('.img');
+imgs.forEach(img => {
+    img.addEventListener('click', function(){
+        value=img.getAttribute('val');
+        console.log(value);
+        slider(value);
+    })
+});
+
+
+document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.slider').style.display="none";
+});
+
+document.querySelector('.left').addEventListener('click', function(){
+    value--;
+    if(value<1){
+        value=8;
+    }
+    slider(value);
+})
+document.querySelector('.right').addEventListener('click', function(){
+    value++;
+    if(value>8){
+        value=1;
+    }
+    slider(value);
+})
